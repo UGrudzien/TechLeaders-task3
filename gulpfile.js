b9f8b140574a904593db299099eaf7b31c552032
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-02-16 21:41:33 +0100

    browserSync task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,40 @@ const gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var autoprefixer=require('gulp-autoprefixer');
+const browserSync  = require('browser-sync').create();
 
 
+const dirs = {
+    assets : './assets',
+    node : './node_modules',
+    dist : './dist'
+};
+const browserSyncOptions = {
+
+    // For more options
+    // @link http://www.browsersync.io/docs/options/
+
+    notify : false,
+
+    // Project URL.
+    // proxy : "sites.dev",
+
+    server : {
+        baseDir : "./"
+    },
+
+    // `true` Automatically open the browser with BrowserSync live server.
+    // `false` Stop the browser from automatically opening.
+    open : true,
+
+    // Inject CSS changes.
+    // Commnet it to relo ad browser for every CSS change.
+    injectChanges : true,
+
+    // Use a specific port (instead of the one auto-detected by Browsersync).
+    // port: 7000,
+
+};
 gulp.task('hello', function(){
     console.log('Hello!');
 })
@@ -16,6 +48,9 @@ gulp.task('styles', function(){
     .pipe(autoprefixer()).
     pipe(gulp.dest('build'));
 })
+gulp.task('browserSync-reload', () => {
+    browserSync.reload();
+})
  
 gulp.task('less', function () {
   return gulp.src('./assets/style/main.less')
@@ -24,10 +59,12 @@ gulp.task('less', function () {
     }))
     .pipe(gulp.dest('./assets/style'));
 });
+gulp.task('browser-sync', () => browserSync.init(browserSyncOptions));
+
 
 // npm run dev | yarn dev
-gulp.task('dev', ['less'], () => {
-    // gulp.watch(`${dirs.assets}/js/**/*.js`, [browserSync.reload]); // Reload on JS file changes.
-    gulp.watch('./assets/style/**/*.less', ['less']); // Reload on SCSS file changes.
-    // gulp.watch('*.html', [browserSync.reload]);
+gulp.task('dev', ['less', 'browser-sync'], () => {
+    gulp.watch(`${dirs.assets}/js/**/*.js`, [browserSync.reload]); // Reload on JS file changes.
+    gulp.watch('./assets/style/**/*.less', ['less', 'browserSync-reload']); // Reload on LeSS file changes.
+    gulp.watch('*.html', [browserSync.reload]);
 });
